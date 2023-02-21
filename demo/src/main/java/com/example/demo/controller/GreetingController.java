86ce62edcfa56efa236e231c82804e8446cfc257
Lisa7676 <122771693+Lisa7676@users.noreply.github.com>
2023-02-21 16:39:22 +0300

    navigation

--- demo/src/main/java/com/example/demo/controller/GreetingController.java
+++ demo/src/main/java/com/example/demo/controller/GreetingController.java
@@ -8,9 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class GreetingController {
 
 	@GetMapping("/")
-	public String greeting( Model model) {
+	public String home( Model model) {
 		model.addAttribute("title", "Гланая страница");
 		return "home";
+	}
+	@GetMapping("/about")
+	public String about( Model model) {
+		model.addAttribute("title", "О нас");
+		return "about";
 	}
 
 }

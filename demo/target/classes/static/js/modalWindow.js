516a502d88e5074bcd04a365ce02db97527fa0f8
Lisa7676 <122771693+Lisa7676@users.noreply.github.com>
2023-03-28 17:10:38 +0500

    modal window

---
+++ demo/target/classes/static/js/modalWindow.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function() {
+
+  var selectCity = getCookie("selectedCity");
+  
+if (selectCity==null) {
+  document.getElementById("city-button").click();
+  
+}
+
+else {
+  var button = document.getElementById("city-button");
+  button.innerHTML = selectCity;}
+
+// Функция для получения значения cookie по имени
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) {
+    return parts.pop().split(";").shift();
+  }
+}
+
+//Сохранение куки по выбранному значению
+$('#save-button').click(function() {
+  var selectedCity = $('#city-select').val();
+
+    document.cookie = "selectedCity=" + selectedCity + "; path=/";
+
+  var selectCity = getCookie("selectedCity");
+  var button = document.getElementById("city-button");
+    button.innerHTML = selectCity;
+  
+});
+
+
+
+});
